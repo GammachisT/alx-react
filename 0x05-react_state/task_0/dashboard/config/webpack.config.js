5572303cd426469c3029e601c7d2f5dfa722ea83
GammachisT <gammachistesfaye@gmail.com>
2024-11-18 04:59:29 -0800

    React state

--- 0x05-react_state/task_0/dashboard/config/webpack.config.js
+++ 0x05-react_state/task_0/dashboard/config/webpack.config.js
@@ -15,7 +15,9 @@ module.exports = {
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: 'babel-loader',
+        use: {
+          loader: 'babel-loader',
+        }
       },
       {
         test: /\.css$/,
